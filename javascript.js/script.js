9cdf0caca31e52ada63ed49aa368caa96b0e4bf3
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-21 17:13:09 +0100

    [UPD] PALINDROMA

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,7 +1,21 @@
 let utente = prompt("Inserisci una parola");
+let parola;
+let controllo = false;
+function palindroma(parola) {
 
-function palindroma(testo) {
-    
+    let risultato =[];
+    for (let i = parola.length - 1 && controllo ===false; i = 0; i--) {
+        risultato += parola[i];
+    }
+    return risultato;
 }
 
-palindroma();
+if (utente == palindroma) {
+    controllo = true;
+}
+if (utente == palindroma) {
+    console.log("la parola è palindroma");
+}
+else {
+    console.log("la parola non è palindroma");
+}
