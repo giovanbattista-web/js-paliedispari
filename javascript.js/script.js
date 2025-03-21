372dd0d3b3b4cdf5d88962d23918956277666939
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-21 17:51:40 +0100

    [UPD] MODIFICA PALINDROMA

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,18 +1,10 @@
 let utente = prompt("Inserisci una parola");
 let parola;
-let controllo = false;
-function palindroma(parola) {
-
-    let risultato =[];
-    for (let i = parola.length - 1 && controllo ===false; i = 0; i--) {
+let risultato =[];
+    for (let i = parola.length - 1; i >= 0; i--) {
         risultato += parola[i];
     }
-    return risultato;
-}
 
-if (utente == palindroma) {
-    controllo = true;
-}
 if (utente == palindroma) {
     console.log("la parola è palindroma");
 }
