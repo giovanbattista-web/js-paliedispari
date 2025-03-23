5e92bb620e0273a2fbdd525f49374662eb54000e
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-23 09:53:57 +0100

    [UPD] NUMERI E SCELTA DEL PARI E DISPARI

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,14 +1,20 @@
+/*
 let utente = prompt("Inserisci una parola");
-    let risultato = '';
-    for (let i = utente.length - 1; i >= 0; i--) {
-        risultato += utente[i];
-    }
+let risultato = "";
+for (let i = utente.length - 1; i >= 0; i--) {
+    risultato += utente[i];
+}
 
-    if (utente == risultato) {
-        console.log("la parola è palindroma");
-    }
-    else {
-        console.log("la parola non è palindroma");
-    }
-   
+if (utente == risultato) {
+    console.log("la parola è palindroma");
+}
+else {
+    console.log("la parola non è palindroma");
+}
+    */
 
+
+let pariDispariNumero = parseInt(prompt("pari o dispari"));
+let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
+let computer = Math.floor(Math.random() * 5) + 1;
+console.log(computer);
