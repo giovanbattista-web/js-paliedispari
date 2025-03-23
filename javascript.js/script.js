f6d61a33915b5dda168875530cbd13e283e9fe3b
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-23 09:57:22 +0100

    [UPD] NUMERO PARI O DISPARI

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -18,3 +18,15 @@ let pariDispariNumero = parseInt(prompt("pari o dispari"));
 let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
 let computer = Math.floor(Math.random() * 5) + 1;
 console.log(computer);
+let result;
+result= numeroUtente + computer;
+console.log(result);
+if(result %2 === 0){
+    console.log("numero pari");
+}
+    else 
+    {
+        console.log("numero dispari");
+}
+
+
