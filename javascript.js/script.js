5859b2c47be5a933d16c61a2944104cd95eb1f82
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-24 08:41:02 +0100

    [UPD] PAROLA PALINDROMA TRAMITE FUNZIONE

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,32 +1,39 @@
-/*
 let utente = prompt("Inserisci una parola");
-let risultato = "";
+let risultato = '';
+function palindroma(nome){
 for (let i = utente.length - 1; i >= 0; i--) {
     risultato += utente[i];
-}
-
+} 
 if (utente == risultato) {
     console.log("la parola è palindroma");
 }
 else {
     console.log("la parola non è palindroma");
 }
-    */
+}
+palindroma(utente);
 
-
+/*
 let pariDispariNumero = parseInt(prompt("pari o dispari"));
 let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
 let computer = Math.floor(Math.random() * 5) + 1;
 console.log(computer);
 let result;
-result= numeroUtente + computer;
+result = numeroUtente + computer;
 console.log(result);
-if(result %2 === 0){
+if (result % 2 === 0) {
     console.log("numero pari");
 }
-    else 
-    {
-        console.log("numero dispari");
+else {
+    console.log("numero dispari");
 }
-
-
+if (result > computer) {
+    console.log("ha vinto l'utente");
+}
+else if (numeroUtente < computer) {
+    console.log("ha vinto il computer");
+}
+else {
+    console.log("Non ha vinto nessuno, entrambi hanno pareggiato");
+}
+*/
